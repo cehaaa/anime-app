430571d4ba6be6465cfb97ade678efa356aa9280
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2022-07-23 11:48:34 +0700

    feat: utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,13 @@
+import { LocalStorage } from "./LocalStorage";
+
 // generate random number range
-export const randomNumber = (min: number, max: number) => {
+export const randomNumber = (min: number, max: number): number => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// find specific element in localStorage
+export const findCollection = (collectionName: string) => {
+	const listOfCollections = new LocalStorage().getCollections();
+
+	return listOfCollections.find(element => element.name === collectionName);
 };
